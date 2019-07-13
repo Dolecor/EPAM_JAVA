cc819341c14a931d9626d4e9dd94e19376d88cf9
Alexey <alexey4sharapov@gmail.com>
2019-07-13 20:44:31 +0300

    Update JPS.java

--- src/main/java/algorithm/JPS.java
+++ src/main/java/algorithm/JPS.java
@@ -10,8 +10,8 @@ import java.util.HashMap;
 
 public class JPS extends Algorithm {
 
-    private Graph graph_;
-    private HashMap<Pair<String, String>, ArrayList<Vertex>> cache_;
+    private Graph graph;
+    private HashMap<Pair<String, String>, ArrayList<Vertex>> cache;
 
 
     public JPS(Graph graph) {
@@ -20,19 +20,19 @@ public class JPS extends Algorithm {
 
     @Override
     public void setGraph(Graph graph) {
-        cache_ = new HashMap<>();
-        graph_ = graph;
+        this.cache = new HashMap<>();
+        this.graph = graph;
     }
 
     @Override
     public Boolean isCalculated(String idSrc, String idDest) {
-        return cache_.containsKey(new Pair<>(idSrc, idDest));
+        return cache.containsKey(new Pair<>(idSrc, idDest));
     }
 
     @Override
     public ArrayList<Vertex> calculate(String idSrc, String idDest)
     {
-        if(isCalculated(idSrc, idDest)) return cache_.get(new Pair<>(idSrc, idDest));
+        if(isCalculated(idSrc, idDest)) return cache.get(new Pair<>(idSrc, idDest));
 
         return null;//It will be changed, when I get how JPS works
     }
