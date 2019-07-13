6142b12dfcf6a945ecac23892593322212dd36d4
Alexey <alexey4sharapov@gmail.com>
2019-07-13 19:59:10 +0300

    Update Algorithm.java

--- src/main/java/algorithm/Algorithm.java
+++ src/main/java/algorithm/Algorithm.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 
 public interface Algorithm {
     void setGraph(Graph graph);
-    //Calculate if isCalculated() is false and return the answer w/o calculation when isCalculated() is true
-    ArrayList<Vertex> calculate();
-    Boolean isCalculated();
+    //Calculate if isCalculated(...) is false and return the answer w/o calculation when isCalculated(...) is true
+    ArrayList<Vertex> calculate(String idSrc, String idDest);
+    Boolean isCalculated(String idSrc, String idDest);
 }
