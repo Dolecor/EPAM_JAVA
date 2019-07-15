89857e58f69a32d1b98dc63f4a2c5d2b0d79314b
Dolenko <dolecor@gmail.com>
2019-07-15 12:22:45 +0300

    WIP: add class BestFirstSearch
    The class represents the algorithm for finding the path from
    the source vertex to the destination vertex.
    Principle (simple): select the next vertex on the principle
    of least weight.
    
    TODO: The class needs to implement methods for backtracking and
    calculating the weight of the path, and then testing the algorithm

---
+++ src/main/java/algorithm/BestFirstSearch.java
@@ -0,0 +1,92 @@
+package algorithm;
+
+import graph.Graph;
+import graph.Vertex;
+import javafx.util.Pair;
+import util.WeightComparator;
+
+import java.util.*;
+
+public class BestFirstSearch implements Algorithm {
+
+
+    private final Graph graph;
+    private Map<Pair<String, String>, ArrayList<Vertex>> cache;
+
+    public BestFirstSearch(Graph graph)
+    {
+        this.graph = graph;
+        this.cache = new HashMap<>();
+    }
+
+    @Override
+    public void setGraph(Graph graph)
+    {
+
+    }
+
+    /**
+     *
+     * @param idSrc identificator of source vertex
+     * @param idDest identificator of destination vertex
+     * @return the list of vertices representing the path from {@code idSrc} to {@code idDest}, or
+     *          {@code null} if there is no path from source vertex to destination vertex
+     */
+    @Override
+    public ArrayList<Vertex> calculate(String idSrc, String idDest)
+    {
+        if(isCalculated(idSrc, idDest)){
+            return cache.get(new Pair<>(idSrc, idDest));
+        }
+
+        ArrayList<Vertex> path = new ArrayList<>();
+        Vertex dest = new Vertex(idDest);
+        Vertex src = new Vertex(idSrc);
+        Vertex curr = src;
+
+        if(idSrc.equals(idDest)){
+            path.add(curr);
+            cache.put(new Pair<>(idSrc, idDest), path);
+            return path;
+        }
+
+        List<Vertex> close = new ArrayList<>();
+        PriorityQueue<Pair<Vertex, Float>> open = new PriorityQueue<>(new WeightComparator());
+        open.add(new Pair<>(curr, -1.0f));
+
+        while(!open.isEmpty()){
+            curr = open.poll().getKey();
+            close.add(curr);
+            if(curr.equals(dest)){
+                path = backTrack(src, dest);
+                cache.put(new Pair<>(idSrc, idDest), path);
+                return path;
+            }
+            for(Pair<Vertex, Float> p : graph.getAdjVertices(curr)){
+                if(!close.contains(p.getKey()) && p.getKey().getPredecessor() == null) {
+                    open.add(p);
+                    p.getKey().setPredecessor(curr);
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Boolean isCalculated(String idSrc, String idDest)
+    {
+        return cache.get(new Pair<>(idSrc, idDest)) != null;
+    }
+
+    // TODO: 15.07.2019  
+    private ArrayList<Vertex> backTrack(Vertex vSrc, Vertex vDest)
+    {
+        return null;
+    }
+
+    // TODO: 15.07.2019
+    public Float getPathWeight(ArrayList<Vertex> path)
+    {
+        return null;
+    }
+}
---
+++ src/main/java/util/WeightComparator.java
@@ -0,0 +1,14 @@
+package util;
+
+import graph.Vertex;
+import javafx.util.Pair;
+
+import java.util.Comparator;
+
+public class WeightComparator implements Comparator<Pair<Vertex, Float>> {
+
+    @Override
+    public int compare(Pair<Vertex, Float> o1, Pair<Vertex, Float> o2) {
+        return o1.getValue().compareTo(o2.getValue());
+    }
+}
