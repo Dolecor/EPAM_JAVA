bc9a7662bbb1da0987d92a9d3a34d0c509006862
LinaVdovichenko <52758279+LinaVdovichenko@users.noreply.github.com>
2019-07-22 12:40:12 +0300

    Add files via upload
    Update BFS.java

--- src/main/java/algorithm/BFS.java
+++ src/main/java/algorithm/BFS.java
@@ -6,7 +6,7 @@ import graph.*;
 import javafx.util.Pair;
 
 import java.util.*;
-/** class - realisation of algorithm BreadthFistSearch
+/** class - realisation of algorithm BestFistSearch
  * has properties <b>graph</b> and <b>cache</b>.
  */
 public class BFS implements Algorithm {
@@ -34,8 +34,8 @@ public class BFS implements Algorithm {
     /** main algo function, which finds best path
      * @param idSrc name of source vertex
      * @param idDest name of destination vertex
-     * @return {@code res} where res keeps value
-     * @return {@code null} if graph don't contain one of given vertexes
+     * @return {@code res} the list of vertices representing the path from {@code idSrc} to {@code idDest}
+     * @return empty list if there is no path from source vertex to destination vertex
      * */
     @Override
     public ArrayList<Vertex> calculate(String idSrc, String idDest) {
@@ -99,7 +99,10 @@ public class BFS implements Algorithm {
     }
 
     /** additional agorithm function
-     * @param path massive of edges
+     * @param path sequence of vertices that represent the path from one vertex to another
+     * @return {@code -1.0f} if {@code path} is empty list, or
+     *          {@code 0.0f} if path contains one vertex, or
+     *          positive float number otherwise.
      */
     @Override
     public Float getPathWeight(ArrayList<Vertex> path)
